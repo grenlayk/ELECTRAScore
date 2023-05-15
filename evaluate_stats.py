f77f0d345de7da7df41b99074b7943d3480483e2
Olga Kozlova <grenlaykk@gmail.com>
2023-05-15 15:00:26 +0100

    Add electra fine-tuning

--- evaluate_stats.py
+++ evaluate_stats.py
@@ -3,9 +3,9 @@ import argparse
 from analysis import SUMStat
 
 
-def main(dataset):
+def main(dataset, input_file):
     if dataset == 'Newsroom' or dataset == 'SummEval':
-        summ_stat = SUMStat(f'data/{dataset}/scores.pkl')
+        summ_stat = SUMStat(f'data/{dataset}/{input_file}')
         summ_stat.evaluate_summary('fluency')
     else:
         print('You used wrong dataset. Please choose Newsroom or SummEval.')
@@ -17,6 +17,11 @@ if __name__ == '__main__':
         '--dataset', type=str, required=True,
         help='The dataset to calculate statistics. Newsroom or SummEval'
     )
+    parser.add_argument(
+        '--input_file', type=str, required=False,
+        default='scores.pkl',
+        help='Name of file to evaluate in dataset folder. Default: scores.pkl'
+    )
 
     args = parser.parse_args()
-    main(args.dataset)
+    main(args.dataset, args.input_file)
